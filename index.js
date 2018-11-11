5e2780fb81a41c93de51254fd9166d134b447c12
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-11-11 16:51:03 -0700

    wh

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ bot.on("guildMemberAdd", function (member) {
         var welcome = new discord.RichEmbed()
             .setTitle("Welcome!")
             .addField("New Member", "New member " + member + " has joined! Give then a warm welcome!", true)
-            .setDescription("You're user " + guild.memberCount)
+            .setDescription("You're user " + guild.id.memberCount)
             .setColor('RANDOM')
             .setThumbnail(member.avatarURL)
             .setFooter("Welcome to the server!")
