52d536bdb7e26828b6cab950c2dd6074dcd69477
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-11-11 13:06:27 -0700

    fix

--- commands/simple/announcements.js
+++ commands/simple/announcements.js
@@ -1,25 +1,22 @@
 const Commando = require('discord.js-commando');
 
-class AnnouncementsCommand extends Commando.Command
-{
-    constructor(client,)
-        {
-        super(client,{
+class AnnouncementsCommand extends Commando.Command {
+    constructor(client, ) {
+        super(client, {
             name: 'announcements',
             group: 'simple',
             memberName: 'announcements',
             description: 'Get notified for new announcements'
         });
     }
-        
-        
-    async run(message, args)
-    {
-        if(message.guild.id == 465707591910162432){
-            let guild = 465707591910162432
-            let announcementRole = guild.roles.find("name", "announcements");
-        message.author.addRole(announcementRole);
-        message.reply('You have recaived the announcements role!');
+
+
+    async run(message, args) {
+        if (message.guild.id == 465707591910162432) {
+            let member = message.author
+            let memberRole = member.guild.roles.find("name", "announcements");
+            member.addRole(memberRole);
+            message.reply('You have recaived the announcements role!');
         }
     }
 }
