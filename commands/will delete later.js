1b9f6644c23c1f9cdad0b0c1a9ac3a899fd5685a
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-11-11 15:18:44 -0700

    delete

--- commands/will delete later.js
+++
@@ -1,9 +0,0 @@
-var welcome = new discord.RichEmbed()
-    .setTitle("Welcome!")
-    .addField("New Member", "New member " + member + " has joined! Give then a warm welcome!", true)
-    .setDescription("You're user 3274732")
-    .setColor('RANDOM')
-    .setThumbnail(membe.avatarURL)
-    .setFooter("Welcome to the server!")
-
-message.channel.send(welcome);
