af9e75ff33e2e1feaa0b13162c79103c78bceeb5
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-11-11 15:35:49 -0700

    ''

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ bot.on("guildMemberAdd", function (member) {
             .addField("New Member", "New member " + member + " has joined! Give then a warm welcome!", true)
             .setDescription("You're user 3274732")
             .setColor('RANDOM')
-            .setThumbnail(member.avatarURL)
+            .setThumbnail('member.avatarURL')
             .setFooter("Welcome to the server!")
 
         channel.send(welcome);
