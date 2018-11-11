49df7964a90cfacb5f1e456f529c974584d2a757
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-11-11 12:56:05 -0700

    f

--- commands/simple/announcements.js
+++ commands/simple/announcements.js
@@ -16,7 +16,7 @@ class AnnouncementsCommand extends Commando.Command
     async run(message, args)
     {
         if(message.guild.id == 465707591910162432){
-            let announcementRole = message.author.guild.roles.find("name", "announcements");
+            let announcementRole = guild.roles.find("name", "announcements");
         message.author.addRole(announcementRole);
         message.reply('You have recaived the announcements role!');
         }
