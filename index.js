06dd20258518f006a4015e696f6c34487d260eb3
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-11-11 16:01:54 -0700

    log member

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ bot.on("guildMemberAdd", function (member) {
 
         channel.send(welcome);
         console.log(thumbnail)
+        console.log(member)
     }
     if (member.guild.id == 510974262769614918) {
         var channel = bot.channels.get('510974262769614921')
