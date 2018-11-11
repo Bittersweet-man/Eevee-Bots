6e8e6d617f160d1ff05e12ed62c16cf4d5de0b3d
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-11-11 15:45:28 -0700

    d

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ bot.on("guildMemberAdd", function (member) {
             .setFooter("Welcome to the server!")
 
         channel.send(welcome);
-        var dm = 462709446121095169
-        dm.send(thumbnail)
+        var channel = 500380461391020042
+        channel.send(thumbnail)
     }
     if (member.guild.id == 510974262769614918) {
         var channel = bot.channels.get('510974262769614921')
