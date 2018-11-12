05ef2431d49d4178fc92b2cdc9942e84050f16fc
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-11-11 21:37:25 -0700

    f

---
+++ commands/admin/createchannel.js
@@ -0,0 +1,23 @@
+const Commando = require('discord.js-commando');
+
+class CreateCommand extends Commando.Command {
+    constructor(client, ) {
+        super(client, {
+            name: 'create',
+            group: 'admin',
+            memberName: 'create',
+            description: 'Create a channel!'
+        });
+    }
+
+
+    async run(message, args) {
+        var channel = message.channel
+        if (!message.member.hasPermission("ADMINISTRATOR")) {
+            message.channel.send("You don't have permissions to use this command!");
+            return;
+        }
+        message.author.guild.createChannel(name, args)
+    }
+}
+module.exports = CreateCommand;
