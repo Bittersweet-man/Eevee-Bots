2757b9687a7d882235388749364c3bd7bd3994a2
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-11-12 01:09:57 -0700

    lyrics

--- commands/simple/lyrics.js
+++ commands/simple/lyrics.js
@@ -14,6 +14,10 @@ class LyricsCommand extends Commando.Command {
 
     async run(message, args) {
         lyr.fetch('twenty one pilots', args, function (err, lyrics) {
+            if(args == null){
+                message.reply('Please specify the song you want the lyrics for!')
+                return;
+            }
             if (lyrics.length > 2000) {
                 lyrics = lyrics.substring(0, 10);
                 message.reply("that song is to long!")
