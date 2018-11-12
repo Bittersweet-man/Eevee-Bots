dc74b0c69baaa9da53a375e58f00ed6940b8d554
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-11-12 14:56:44 -0700

    kk

--- commands/simple/doggo.js
+++ commands/simple/doggo.js
@@ -28,7 +28,7 @@ class DoggoCommand extends Commando.Command {
             .setAuthor('Sylveon Bot', message.guild.iconURL)
             .setImage(body.message)
             .setTimestamp()
-            .setFooter('Sylveon Bot',  bot.user.displayAvatarURL)
+            .setFooter('Sylveon Bot')
             .setColor('RANDOM')
             // bot.user.displayAvatarURL
 
