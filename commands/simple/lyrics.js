61a32d82516f32c1d04a841d2535571736dcc39c
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-11-12 01:02:13 -0700

    dbd

--- commands/simple/lyrics.js
+++ commands/simple/lyrics.js
@@ -1,27 +1,27 @@
 const Commando = require('discord.js-commando');
 var lyr = require('lyrics-fetcher');
 
-class LyricsCommand extends Commando.Command
-{
-    constructor(client,)
-        {
-        super(client,{
+class LyricsCommand extends Commando.Command {
+    constructor(client, ) {
+        super(client, {
             name: 'lyrics',
             group: 'simple',
             memberName: 'lyrics',
             description: 'Get lyrics!(Of Sting)'
         });
     }
-        
-        
-    async run(message, args)
-    {
+
+
+    async run(message, args) {
         lyr.fetch('twenty one pilots', args, function (err, lyrics) {
-            if(lyrics.length > 2000) lyrics = lyrics.substring(0,10);{
+            if (lyrics.length > 2000) {
+                lyrics = lyrics.substring(0, 10);
                 message.reply("that song is to long!")
                 return;
+            } {
+                message.channel.send(err || lyrics);
             }
-            message.channel.send(err || lyrics);
+
         });
     }
 }
