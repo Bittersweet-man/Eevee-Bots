65a2ffc37c42809d76a3278d4ff2788983f596c5
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-11-12 21:07:17 -0700

    hd

--- commands/simple/coin_flip.js
+++ commands/simple/coin_flip.js
@@ -21,15 +21,15 @@ class CoinFlipCommand extends Commando.Command {
             var results = 'tails'
             //message.reply("Your coin landed on Tails!", {files: [__dirname + "/CoinPics/tails.jpg"]});;
         }
-        if(results == 'tails'){
-            var image = 'https://www.google.com/search?rlz=1C1GCEU_enUS821US821&biw=1360&bih=626&tbm=isch&sa=1&ei=3EzqW_OOL4fI8APZ37rACg&q=coin+tai%3Bs+png&oq=coin+tai%3Bs+png&gs_l=img.3...44755.45388..45581...0.0..0.158.505.3j2......1....1..gws-wiz-img.......0i7i30j0i8i7i30.HqthOGD1WNc#imgrc=3P9luPC6xgGO_M:'
+        if (results == 'tails') {
+            var image = 'https: //mbtskoudsalg.com/images/quarter-tail-png-7.png '
+
+        } else {
+            var image = 'https://bjc.edc.org/June2017/bjc-r/img/5-algorithms/img_flipping-a-coin/Heads.png'
         }
-        else{
-            var image = 'https://www.google.com/search?q=coin+heads+png&rlz=1C1GCEU_enUS821US821&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjJqqqPvtDeAhUjwlQKHU5tAvQQ_AUIEygB&biw=1360&bih=626#imgrc=wcq63vmxowogrM:'
-        }
-       
-       
-       
+
+
+
         let fEmbed = new discord.RichEmbed()
             .setTitle('Flip')
             .addField('Result', 'Your coin landed on ' + results)
