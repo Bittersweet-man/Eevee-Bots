9cd2d1b549ba4640274de3cc9909d11c4acfc980
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-11-12 20:57:03 -0700

    free to go

--- commands/simple/coin_flip.js
+++ commands/simple/coin_flip.js
@@ -24,7 +24,7 @@ class CoinFlipCommand extends Commando.Command {
         let fEmbed = new discord.RichEmbed()
             .setTitle('Flip')
             .addField('Result', 'Your coin landed on ' + results)
-            .addField('Requested by' + message.author + ' requested this!')
+            .addField('Requested by', message.author)
             .setDescription("A coin was flipped!")
             .setTimestamp()
             .setColor('RANDOM')
