526781cc40e6d6c13c7a794adfd336f1264155f4
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-11-12 20:41:47 -0700

    flip embed

--- commands/simple/coin_flip.js
+++ commands/simple/coin_flip.js
@@ -1,28 +1,36 @@
 const Commando = require('discord.js-commando');
+const discord = require('discord.js');
 
-class CoinFlipCommand extends Commando.Command
-{
-    constructor(client)
-        {
-        super(client,{
+class CoinFlipCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
             name: 'flip',
             group: 'simple',
             memberName: 'flip',
             description: 'Flips a coin, landing on either Heads or Tails'
         });
     }
-        
-        
-    async run(message, args)
-    {
+
+
+    async run(message, args) {
         var chance = Math.floor(Math.random() * 2)
-        if(chance == 0){
-            message.reply("Your coin landed on Heads!", {files: [__dirname + "/CoinPics/heads.jpg"]});
+        if (chance == 0) {
+            var results = 'heads'
+            // message.reply("Your coin landed on Heads!", {files: [__dirname + "/CoinPics/heads.jpg"]});
+        } else {
+            var results = 'tails'
+            //message.reply("Your coin landed on Tails!", {files: [__dirname + "/CoinPics/tails.jpg"]});;
         }
-        else
-        { 
-            message.reply("Your coin landed on Tails!", {files: [__dirname + "/CoinPics/tails.jpg"]});;
-        }
+        let fEmbed = new discord.RichEmbed()
+            .setTitle('Flip')
+            .addField('Result', 'Your coin landed on ' + results)
+            .setDescription("A coin was flipped!")
+            .setTimestamp()
+            .setColor('RANDOM')
+            .setFooter('Requested by ' + message.author)
+        channel.send({
+            embed: fEmbed
+        })
     }
 }
 
