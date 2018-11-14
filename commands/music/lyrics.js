40f9dff76226af1bc938c515bc3964a72d238d8b
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-11-13 20:34:59 -0700

    djdj

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -17,7 +17,7 @@ class LyricsCommand extends Commando.Command {
         var id = message.author.id
         message.reply('What artist are you looking for?')
         const msgs = await message.channel.awaitMessages(msg => {
-            if (message.author.id = id) {
+            if (message.author.id !== '511006054495289354') {
                 console.log(msg.content);
                 message.channel.send('Your artist is ' + msg.content)
             }
