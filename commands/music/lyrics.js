7c6c942251b29569eb34080a8569929e124e7af3
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-11-13 20:03:49 -0700

    artist?

---
+++ commands/music/lyrics.js
@@ -0,0 +1,47 @@
+const Commando = require('discord.js-commando');
+var lyr = require('lyrics-fetcher');
+
+class LyricsCommand extends Commando.Command {
+    constructor(client, ) {
+        super(client, {
+            name: 'lyrics',
+            group: 'music',
+            memberName: 'lyrics',
+            description: 'Get lyrics!(Of Sting)'
+        });
+    }
+
+
+    async run(message, args) {
+    message.reply('What artist are you looking for?')
+    const msgs = await message.channel.awaitMessages(msg => { 
+        console.log(msg.content) ;
+        message.channel.send('Your artist is ' + msgs)
+    }, {time: 5000});
+
+       //lyr.fetch(artist, song, function (err, lyrics) {
+          //  if (args == null) {
+            //    message.reply('Please specify the song you want the lyrics for!')
+
+            //}
+        //    if (lyrics.length > 2000) {
+          //     var lyrics = lyrics.substring(0, 2000);
+            //    let words = lyrics.split(2000);
+//                message.channel.send('```' + words + '```')
+  //          } {
+    //            if (lyrics.length < 2000) {
+      //              message.channel.send('```' + lyrics + '```');
+        //        }
+          //  } {
+            //    if (lyrics.length > 2000) {
+              //    var lyrics2 = lyrics.slice(-1000);
+                //    message.channel.send('```' + lyrics2 + '```')
+
+//                }
+  //          }
+
+   //     });
+    }
+}
+
+module.exports = LyricsCommand;
