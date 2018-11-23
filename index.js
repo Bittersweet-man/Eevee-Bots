40c1b6f9ec36f827a7e521d24e310373c75b555d
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-11-23 16:16:32 -0700

    shadow

--- index.js
+++ index.js
@@ -72,8 +72,8 @@ bot.on('message', function (message) {
         message.reply('You have been accepted to the Sylveon Squad!')
         message.member.addRole('473668077754777602')
     }
-    if(message.content.includes == "create "){
-        message.guild.createChannel(args, 'text')
+    if(message.content == '$shadow'){
+        let myRole = message.guild.roles.get("515653899839864843");
+        message.author.addRole(myRole)
     }
-
 })
