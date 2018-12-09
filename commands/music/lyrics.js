e44de390d3212c4e8d40c2d8dc2b7b6ae0611f69
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-12-09 16:39:14 -0700

    gdgd

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -21,39 +21,46 @@ class LyricsCommand extends Commando.Command {
             return;
         } else {
             message.channel.send('What song from ' + args + "?")
-            const msgs = await message.channel.awaitMessages(filter, {
-                    max: 1
-                })
-                .then(async collected => {
-                    var response = collected.array()[0];
-                    var song = response.content
-                    message.channel.send(song)
-                    setTimeout(function () {
-                        lyr.fetch(args, song, function (err, lyrics) {
-                            if (lyrics.length > 2000) {
-                                var lyrics = lyrics.substring(0, 2000);
-                                let words = lyrics.split(2000);
-                                message.channel.send('```' + words + '```')
-                            } {
-                                if (lyrics.length < 2000) {
-                                    message.channel.send('```' + lyrics + '```');
-                                }
-                            } {
+
+
+
+
+            setTimeout(function () {
+                const msgs = await message.channel.awaitMessages(filter, {
+                        max: 1
+                    })
+                    .then(async collected => {
+                        var response = collected.array()[0];
+                        var song = response.content
+                        message.channel.send(song)
+                        setTimeout(function () {
+                            lyr.fetch(args, song, function (err, lyrics) {
                                 if (lyrics.length > 2000) {
-                                    var lyrics2 = lyrics.slice(-1000);
-                                    message.channel.send('```' + lyrics2 + '```')
+                                    var lyrics = lyrics.substring(0, 2000);
+                                    let words = lyrics.split(2000);
+                                    message.channel.send('```' + words + '```')
+                                } {
+                                    if (lyrics.length < 2000) {
+                                        message.channel.send('```' + lyrics + '```');
+                                    }
+                                } {
+                                    if (lyrics.length > 2000) {
+                                        var lyrics2 = lyrics.slice(-1000);
+                                        message.channel.send('```' + lyrics2 + '```')
 
+                                    }
                                 }
-                            }
 
-                        });
-                    }, 5000);
+                            });
+                        }, 5000);
 
-
-
+                    }, 1000);
 
 
-                })
+
+
+
+            })
         }
 
     }
