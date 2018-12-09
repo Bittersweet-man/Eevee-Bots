ddc59d175b59e828c54be32c3e5bc01044d5df89
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-12-09 13:57:16 -0700

    wh

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -14,6 +14,7 @@ class LyricsCommand extends Commando.Command {
 
 
     async run(message, args) {
+        message.channel.send("test")
         const msgs = await message.channel.awaitMessages(msg => {
             if(msgs.content != null){} return;
         }, {
