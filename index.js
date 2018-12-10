9a7f01b9f89fe2e83ee6c489259706e95e986953
Bittersweet-man <44100190+Bittersweet-man@users.noreply.github.com>
2018-12-09 17:23:43 -0700

    hsh

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Commando = require('discord.js-commando');
-const TOKEN = "NTExMDA2MDU0NDk1Mjg5MzU0.Du87nQ.RuAhIqGzUQgg_Swhrvr_EiL87v8"
-//process.env.TOKEN;
+const TOKEN = process.env.TOKEN;
 const bot = new Commando.Client({
     commandPrefix: "?",
     owner: "462709446121095169",
